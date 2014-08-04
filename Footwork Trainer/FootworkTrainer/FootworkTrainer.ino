a78fa89c181f99fdac0555fd688121ee23a48003
Joey Rafidi <jrafidi@palantir.com>
2014-08-04 08:26:36 -0700

    More footwork trainer code

--- Footwork Trainer/FootworkTrainer/FootworkTrainer.ino
+++ Footwork Trainer/FootworkTrainer/FootworkTrainer.ino
@@ -8,24 +8,40 @@
 Servo myservo;  // create servo object to control a servo 
                 // a maximum of eight servo objects can be created 
  
-int pos = 0;    // variable to store the servo position 
+int lowLimit = 50;
+int highLimit = 130;
+int pos = 90;
+int delayStep = 200;
+
+int speedLimit = 100;
  
 void setup() 
 { 
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  Serial.begin(9600);
 } 
  
  
 void loop() 
 { 
-  for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees 
-  {                                  // in steps of 1 degree 
-    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(15);                       // waits 15ms for the servo to reach the position 
-  } 
-  for(pos = 180; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees 
-  {                                
-    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(15);                       // waits 15ms for the servo to reach the position 
-  } 
+  myservo.write(pos);
+  
+  int stepSize = (int) (analogRead(1) / 100);
+
+  int nextStep;
+  if (pos <= lowLimit) {
+    nextStep = stepSize;
+  } else if (pos >= highLimit) {
+    nextStep = -1 * stepSize;
+  } else {
+    long seed = random(2);
+    if (seed == 1) {
+      nextStep = stepSize;
+    } else {
+      nextStep = -1 * stepSize;
+    }
+  }
+  
+  pos = pos + nextStep;
+  delay(max(analogRead(0), speedLimit));
 } 
