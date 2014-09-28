023dc3db0ec724e2736e41509b05448338019378
Joey Rafidi <jrafidi@palantir.com>
2014-09-28 09:32:17 -0700

    Update footwork trainer arduino code with more control

--- Footwork Trainer/FootworkTrainer/FootworkTrainer.ino
+++ Footwork Trainer/FootworkTrainer/FootworkTrainer.ino
@@ -1,37 +1,46 @@
-// Sweep
-// by BARRAGAN <http://barraganstudio.com> 
-// This example code is in the public domain.
-
-
 #include <Servo.h> 
  
-Servo myservo;  // create servo object to control a servo 
-                // a maximum of eight servo objects can be created 
- 
-int lowLimit = 50;
-int highLimit = 130;
+Servo myservo;
+
+int SERVO_PIN = 9;
+int SPEED_PIN = 0;
+int SPEED_VARIANCE_PIN = 1;
+int STEP_SIZE_PIN = 2;
+int RANGE_PIN = 3;
+
+float limitDenominator = 1024/60;
+
+int midPoint = 90;
+int limitMin = 15;
 int pos = 90;
 int delayStep = 200;
-
 int speedLimit = 100;
+
+boolean firstPass = true;
  
 void setup() 
 { 
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
-  Serial.begin(9600);
-} 
- 
+  myservo.attach(SERVO_PIN);
+}
  
 void loop() 
 { 
   myservo.write(pos);
   
-  int stepSize = (int) (analogRead(1) / 100);
+  if (firstPass) {
+    delay(5000);
+    firstPass = false;
+  }
+
+  int stepSize = (int) (analogRead(STEP_SIZE_PIN) / 100);
+  stepSize = max(stepSize, 1);
+
+  int limit = (int) (analogRead(RANGE_PIN) / limitDenominator);
 
   int nextStep;
-  if (pos <= lowLimit) {
+  if (pos <= (midPoint - limit - limitMin)) {
     nextStep = stepSize;
-  } else if (pos >= highLimit) {
+  } else if (pos >= (midPoint + limit + limitMin)) {
     nextStep = -1 * stepSize;
   } else {
     long seed = random(2);
@@ -41,7 +50,16 @@ void loop()
       nextStep = -1 * stepSize;
     }
   }
-  
   pos = pos + nextStep;
-  delay(max(analogRead(0), speedLimit));
+
+  int delayTime = analogRead(SPEED_PIN);
+  int timeVariance = (int) random(analogRead(SPEED_VARIANCE_PIN));
+  long seed = random(2);
+  if (seed == 1) {
+    delayTime = delayTime + timeVariance;
+  } else {
+    delayTime = delayTime - timeVariance;
+  }
+
+  delay(max(delayTime, speedLimit));
 } 
